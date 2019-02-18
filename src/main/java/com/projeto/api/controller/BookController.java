7ce5f1ddfe931598a9b456b70c8bdba1f1d4b432
Rodrigo Ribeiro <rodrigo.ribeiro03@hotmail.com>
2019-02-18 16:46:14 -0300

    buscando ISBN teste

--- src/main/java/com/projeto/api/controller/BookController.java
+++ src/main/java/com/projeto/api/controller/BookController.java
@@ -42,11 +42,21 @@ public class BookController {
 			Elements descriptions = doc.select("p");
 			Elements languages = doc.select("div [class=\"book-lang\"]");
 			Elements links = doc.select("p a");	
+			System.out.println(links.attr("href"));
 			Book book = new Book();
 			for (Element link : links ) {
 				Document doc2 = Jsoup.connect(link.attr("href")).get();
-				Element isbn = doc.;
-				System.out.println(isbn);
+				if(link.attr("href").substring(11,22).equals("fundamental")) {
+					Elements isbn = doc.select("h2 [class=dark-blue-text] span");
+					
+				}else if(link.attr("href").substring(11,18).equals("manning")){
+					
+				}else if(link.attr("href").substring(11,17).equals("amazon")){
+					
+				}else if(link.attr("href").substring(11,19).equals("packtpub")){
+					
+				}
+				
 				
 			}
 
