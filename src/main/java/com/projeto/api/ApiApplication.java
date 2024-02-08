8c34ee60b8937a033640d9bf596d562431c7452d
Rodrigo Ribeiro <rodrigo.ribeiro03@hotmail.com>
2024-02-08 10:02:05 -0300

    adicionando H2 na inicializacao da app

--- src/main/java/com/projeto/api/ApiApplication.java
+++ src/main/java/com/projeto/api/ApiApplication.java
@@ -3,11 +3,29 @@ package com.projeto.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+		try {
+            // Carregar o driver do H2
+            Class.forName("org.h2.Driver");
+
+            // Conectar ao banco de dados em memória
+            Connection connection = DriverManager.getConnection("jdbc:h2:mem:testdb", "sa", "");
+
+            // Execute operações no banco de dados, crie tabelas, etc.
+
+            // Fechar a conexão quando não for mais necessário
+            connection.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
 	}
 
 }
